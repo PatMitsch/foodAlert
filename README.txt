902ccef01d9f7f37e6673283e0c1471e2c568dad
PatMitsch <patmitsch@gmail.com>
2014-02-01 22:07:22 -0500

    initial check in
    Just setting up GitHub and hello world version of foodAlert

---
+++ README.txt
@@ -0,0 +1,5 @@
+A simple Python (3.3) script that will monitor the status of a 
+big red button that is connected to a Raspberry Pi.
+
+When the button is pressed, the script will send out an email
+to a pre-determined address.
---
+++ foodAlert.py
@@ -0,0 +1,22 @@
+# Code to run food alert in Python
+
+#import RPi.GPIO
+
+
+
+#function to monitor button
+def monitorButton():
+    print ( "Not set up yet" )
+    
+# function to send email out
+def shoutOut():
+    print ( "Not set up yet" )
+
+    
+#main
+def main():
+    print ( "What's up, world?" )
+
+
+if __name__ == "__main__":
+    main()
